04fd45560edca81950a84d752d7c63536ca8df9c
Sudipto Chowdhury <dip.acm77@gmail.com>
2019-06-04 14:16:51 +0600

    added redux-logger for state logging and redux-thunk middleware

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import { searchRobots } from './reducers';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
+import { searchRobots, requestRobots } from './reducers';
 import './index.css';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 
-const store = createStore(searchRobots);
+const logger = createLogger();
+const rootReducers = combineReducers({ searchRobots, requestRobots });
+const store = createStore(rootReducers, applyMiddleware(thunkMiddleware, logger));
 
 ReactDOM.render(
     <Provider store={store}>
