ab88b2c0b527ce2ccb448e866c52f9963b9e7899
Sudipto Chowdhury <dip.acm77@gmail.com>
2019-06-07 00:36:37 +0600

    Updates

---
+++ precache-manifest.2ce496269330da398e9a5d08031b097e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7403808075fcff379be9d0befac74317",
+    "url": "/react-robofriends/index.html"
+  },
+  {
+    "revision": "dfe41dc8d3cd28d86585",
+    "url": "/react-robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "c743646f8910ef0d5b98",
+    "url": "/react-robofriends/static/css/main.27f94a5a.chunk.css"
+  },
+  {
+    "revision": "dfe41dc8d3cd28d86585",
+    "url": "/react-robofriends/static/js/2.c7fac639.chunk.js"
+  },
+  {
+    "revision": "c743646f8910ef0d5b98",
+    "url": "/react-robofriends/static/js/main.bf515223.chunk.js"
+  },
+  {
+    "revision": "238297068e2e665c321e",
+    "url": "/react-robofriends/static/js/runtime~main.41660cae.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/react-robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
