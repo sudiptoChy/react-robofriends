5f768a10cb63cff91f614c2fc7a4b5524b4904a2
Sudipto Chowdhury <dip.acm77@gmail.com>
2019-06-07 01:50:44 +0600

    fixed performance issues and added PWA functionalities

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Scroll.css';
 
 const Scroll = (props) => {
     return (
-        <div style={{overflowY: 'scroll', border: '5px solid black', height: '500px'}}>
+        <div className="overflow">
             {props.children}
         </div>
     );
--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -4,7 +4,8 @@ import './SearchBox.css';
 const SearchBox = ({searchField, searchChange}) => {
     return (
         <div className="pa2 searchbox">
-            <input style={{width: '50%'}}
+            <input style={{ width: '50%' }}
+                aria-label="Search Robots"
                 className="pa3 ba b--green bg-lightest-blue"
                 type="search"
                 placeholder="Search Robots"
