12ee80e7b228f7e21c74fc73dae42d8248b44164
Sudipto Chowdhury <dip.acm77@gmail.com>
2019-06-07 00:53:07 +0600

    Updates

--- precache-manifest.e1d6376480deb6c3537479cb31bab317.js
+++ precache-manifest.e1d6376480deb6c3537479cb31bab317.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7403808075fcff379be9d0befac74317",
+    "revision": "d8584e651d54a5e3e513f8213812d421",
     "url": "/react-robofriends/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-robofriends/static/css/2.31dafeb2.chunk.css"
   },
   {
-    "revision": "c743646f8910ef0d5b98",
+    "revision": "fd7b0098686befd10e10",
     "url": "/react-robofriends/static/css/main.27f94a5a.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-robofriends/static/js/2.c7fac639.chunk.js"
   },
   {
-    "revision": "c743646f8910ef0d5b98",
-    "url": "/react-robofriends/static/js/main.bf515223.chunk.js"
+    "revision": "fd7b0098686befd10e10",
+    "url": "/react-robofriends/static/js/main.bebeb2a3.chunk.js"
   },
   {
     "revision": "238297068e2e665c321e",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-robofriends/precache-manifest.2ce496269330da398e9a5d08031b097e.js"
+  "/react-robofriends/precache-manifest.e1d6376480deb6c3537479cb31bab317.js"
 );
 
 self.addEventListener('message', (event) => {
